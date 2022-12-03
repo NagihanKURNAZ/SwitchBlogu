64027b00db360e50cb657600abeaa7baafdaaffc
NagihanKURNAZ <112775522+NagihanKURNAZ@users.noreply.github.com>
2022-12-03 14:04:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		char grade = 'C';
+		
+		switch(grade) {
+		     case 'A':
+		    	 System.out.println("Mükemmel: Geçti");
+		         break;
+		     case 'B':
+		    	 System.out.println("Çok Güzel: Geçtiniz");
+		    	 break;
+		     case 'C':
+		    	 System.out.println("İyi: Geçtiniz");
+		    	 break;
+		     case 'D':
+		    	 System.out.println("Fena Değil: Geçtiniz");
+		    	 break;
+		     case 'F':
+		    	 System.out.println("Kladınız");
+		    	 break; 
+		    	 default:
+		    		 System.out.println("Geçersiz not girdiniz");
+		}
+		
+		
+	}
+
+}
